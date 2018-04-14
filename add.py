ceb743f52a157eafd478a0ccd60ca85f618a7e71
akruman <akruman@gmail.com>
2018-04-14 19:20:59 +0300

    unicode

--- add.py
+++ add.py
@@ -1,10 +1,10 @@
-import random
+﻿import random
 for i in range(5):
     a = int(random.random() * 200)
     b = int(random.random() * 200)
     print '{0}. Maayan how much is: {1}+{2}=?'.format(i,a,b)
     x=raw_input()
     if int(x) == a+b:
-        print '?? ?????'
+        print 'כל הכבוד'
     else:
-        print '?? ????'
+        print 'לא נכון'
